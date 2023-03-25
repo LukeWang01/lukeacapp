eac05be68d4331cf32dbcb07cbda14b15f15f8f2
Luke <wangzilu9488@gmail.com>
2023-03-26 07:30:28 +0800

    add login and register

--- lukeacgame/static/js/dist/game.js
+++ lukeacgame/static/js/dist/game.js
@@ -568,6 +568,115 @@ class Settings {
         this.username = "";
         this.photo = "";
 
+        this.$settings = $(`
+<div class="ac-game-settings">
+    <div class="ac-game-settings-login">
+        <div class="ac-game-settings-title">
+            Log In
+        </div>
+
+        <div class="ac-game-settings-username">
+            <div class="ac-game-settings-item">
+                <input type="text" placeholder="User Name" name="username">
+            </div>
+        </div>
+
+        <div class="ac-game-settings-password">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="Password">
+            </div>
+        </div>
+
+        <div class="ac-game-settings-submit">
+            <div class="ac-game-settings-item">
+                <button> Confirm </button>
+            </div>
+        </div>
+
+        <div class="ac-game-settings-error-message">
+            Log in error: username or password is incorrect!
+        </div>
+        
+        <br>
+        
+        <div class="ac-game-settings-option-register">
+            Register
+        </div>
+        
+        <br>
+
+        <div class="ac-game-settings-acwing">
+            <div class="ac-game-settings-acwing-loginwith">
+                Log in With
+            </div>
+            <br>
+            <img width="30" src="https://app5069.acapp.acwing.com.cn/static/image/settings/ac_logo.png">
+        </div>
+
+    </div>
+
+    <div class="ac-game-settings-register">
+        <div class="ac-game-settings-title">
+            Register
+        </div>
+
+        <div class="ac-game-settings-username">
+            <div class="ac-game-settings-item">
+                <input type="text" placeholder="User Name" name="username">
+            </div>
+        </div>
+
+        <div class="ac-game-settings-password">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="Password">
+            </div>
+        </div>
+
+        <div class="ac-game-settings-password">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="Reenter Password">
+            </div>
+        </div>
+
+        <div class="ac-game-settings-submit">
+            <div class="ac-game-settings-item">
+                <button> Confirm </button>
+            </div>
+        </div>
+
+        <div class="ac-game-settings-error-message">
+            Log in error: username or password is incorrect!
+        </div>
+        
+        <br>
+        
+        <div class="ac-game-settings-option-register">
+            Log In
+        </div>
+        
+        <br>
+
+        <div class="ac-game-settings-acwing">
+            <div class="ac-game-settings-acwing-loginwith">
+                Log in With
+            </div>
+            <br>
+            <img width="30" src="https://app5069.acapp.acwing.com.cn/static/image/settings/ac_logo.png">
+        </div>
+
+    </div>
+
+
+</div>
+            `);
+        this.$login = this.$settings.find(".ac-game-settings-login");
+        this.$login.hide();
+
+        this.$register =this.$settings.find(".ac-game-settings-register");
+        this.$register.hide();
+
+        this.root.$ac_game.append(this.$settings);
+
         this.start();
     }
 
@@ -576,12 +685,14 @@ class Settings {
     }
 
     login() {
-
+        this.$register.hide();
+        this.$login.show();
     }
 
 
     register() {
-
+        this.$login.hide();
+        this.$register.show();
     }
 
 
@@ -601,7 +712,9 @@ class Settings {
                     outer.hide();
                     outer.root.menu.show();
                 } else {
-                    outer.login();
+                    // open lonin by default
+                    //outer.login();
+					outer.register();
                 }
             }
         });
@@ -609,11 +722,11 @@ class Settings {
     }
 
     show() {
-
+        this.$settings.show();
     }
     
     hide() {
-    
+        this.$settings.hide();
     }
 
 }
